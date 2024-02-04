1ecb016019ec3dcfa527419bccd6aae4050ff32e
DIPANKAR BARMAN <barman498@gmail.com>
2024-02-05 00:40:33 +0530

    feat: toolbar states added

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,9 +1,17 @@
 'use client'
 
-import { useSelf } from '@/liveblocks.config'
+import { useState } from 'react'
+
+import {
+    useCanRedo,
+    useCanUndo,
+    useHistory,
+    useSelf,
+} from '@/liveblocks.config'
 import Info from './info'
 import Participants from './participants'
 import Toolbar from './toolbar'
+import { CanvasMode, CanvasState } from '@/types/canvas'
 
 interface CanvasProps {
     boardId: string
@@ -11,12 +19,26 @@ interface CanvasProps {
 
 const Canvas = ({ boardId }: CanvasProps) => {
     const info = useSelf((me) => me.info)
+    const history = useHistory()
+    const canUndo = useCanUndo()
+    const canRedo = useCanRedo()
+
+    const [canvasState, setCanvasState] = useState<CanvasState>({
+        mode: CanvasMode.None,
+    })
 
     return (
-        <div className="h-full w-full relative bg-natural-100 touch-none">
+        <div className="h-full w-full relative bg-natural-500 touch-none">
             <Info boardId={boardId} />
             <Participants />
-            <Toolbar />
+            <Toolbar
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                canRedo={canRedo}
+                canUndo={canUndo}
+                undo={history.undo}
+                redo={history.redo}
+            />
         </div>
     )
 }
